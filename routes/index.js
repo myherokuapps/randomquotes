be730f0a9df755e154a2cf96a58527ce77bb1ec8
apoorvanand <apoorwanand@gmail.com>
2018-06-10 21:06:56 +0530

    updated with routes

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My Random quotes Api' });
+  res.render('index', { title: ' My Random quotes' });
 });
 
 module.exports = router;
